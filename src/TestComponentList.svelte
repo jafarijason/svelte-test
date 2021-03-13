74dde4f2dae4c6b992159c2f7141ab689d7a7b18
Jafari Jason <eng.jafari.m@gmail.com>
2021-03-13 13:52:12 -0500

    play with components

---
+++ src/TestComponentList.svelte
@@ -0,0 +1,15 @@
+<script>
+	let secodName = 'Svelte test';
+	let srcSvelte = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/1200px-Svelte_Logo.svg.png';
+</script>
+
+<style lang="scss">
+	.main-img{
+		width: 30%;
+	}
+</style>
+
+
+<img src="{srcSvelte}" class="main-img" />
+<h2 class=" mb-0">{secodName}</h2>
+
---
+++ src/TestSearch.svelte
@@ -0,0 +1,9 @@
+<script>
+    // your script goes here
+    export let search;
+</script>
+
+<h4 class="text-primary" >
+    search:
+    <small>for: {search} </small>
+</h4>
