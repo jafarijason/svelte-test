f988908d6501a7b52e5c70cd6232b24426972af1
Jafari Jason <eng.jafari.m@gmail.com>
2021-03-13 13:12:59 -0500

    play with bootstap scss and rewite some varible of bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,25 @@
 
 <style lang="scss">
 	$color: red;
+	$danger: gray;
+
+	@import '../node_modules/bootstrap/scss/bootstrap.scss';
 
 	h1 {
 		p{
 			font-size: xx-large;
+			color: $warning;
 		}
-		color: $color;
+
+		span{
+			color: $danger;
+		};
+		color: $primary;
 	}
 </style>
 
+<div class="container">
+	<h1><p>Hello </p>{name} <span>!</span></h1>
+	<p class="text-danger">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
 
-<h1><p>Hello {name}!</p></h1>
