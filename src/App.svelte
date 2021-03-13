f4d256c8676503ed9241f4eb3944866352ea1cd2
Jafari Jason <eng.jafari.m@gmail.com>
2021-03-13 13:31:59 -0500

    use some varible for dom

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
 	export let name;
+	let secodName = 'Svelte test';
+	let srcSvelte = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/1200px-Svelte_Logo.svg.png';
+
 </script>
 
 <style lang="scss">
@@ -19,10 +22,19 @@
 		};
 		color: $primary;
 	}
+
+	.main-img{
+		width: 30%;
+	}
+
+
 </style>
 
 <div class="container">
 	<h1><p>Hello </p>{name} <span>!</span></h1>
 	<p class="text-danger">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+	<img src="{srcSvelte}" class="main-img" >
+	<h2 class=" mb-0">{secodName}</h2>
 </div>
 
